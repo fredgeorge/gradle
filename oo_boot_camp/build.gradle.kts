b1c5006c783a8eeff658c4c0f05acbd268d1c7f1
Fred George <fredgeorge@acm.org>
2021-07-20 14:04:11 +0200

    Fred: Fixed bug in build.gradle.kts; fixed package names to conform to Detekt

--- oo_boot_camp/build.gradle.kts
+++ oo_boot_camp/build.gradle.kts
@@ -1,3 +1,13 @@
+/*
+ * Copyright (c) 2021 by Fred George
+ * May be used freely except for training; license required for training.
+ * @author Fred George  fredgeorge@acm.org
+ */
+
+repositories {
+    // Use Maven Central for resolving dependencies.
+    mavenCentral()
+}
 
 plugins {
     // Apply the org.jetbrains.kotlin.jvm Plugin to add support for Kotlin.
